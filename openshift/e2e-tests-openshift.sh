e659fa2093b463830fd4c5ff6fa98a7ec5993017
David Simansky <dsimansk@redhat.com>
2020-05-28 13:40:42 +0200

    Update E2E setup to Serverless 1.8 + Eventing 0.14 (#371)

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -21,14 +21,13 @@ set -x
 
 readonly KN_DEFAULT_TEST_IMAGE="gcr.io/knative-samples/helloworld-go"
 readonly SERVING_NAMESPACE="knative-serving"
-readonly EVENTING_NAMESPACE="knative-eventing"
+readonly SERVICEMESH_NAMESPACE="knative-serving-ingress"
 readonly E2E_TIMEOUT="60m"
 readonly OLM_NAMESPACE="openshift-marketplace"
-readonly EVENTING_CATALOGSOURCE="https://raw.githubusercontent.com/openshift/knative-eventing/master/openshift/olm/knative-eventing.catalogsource.yaml"
 env
 
 # Loops until duration (car) is exceeded or command (cdr) returns non-zero
-function timeout_non_zero() {
+function timeout() {
   SECONDS=0; TIMEOUT=$1; shift
   while eval $*; do
     sleep 5
@@ -43,8 +42,7 @@ function scale_up_workers(){
   oc get machineset -n ${cluster_api_ns} --show-labels
 
   # Get the name of the first machineset that has at least 1 replica
-  local machineset
-  machineset=$(oc get machineset -n ${cluster_api_ns} -o custom-columns="name:{.metadata.name},replicas:{.spec.replicas}" | grep " 1" | head -n 1 | awk '{print $1}')
+  local machineset=$(oc get machineset -n ${cluster_api_ns} -o custom-columns="name:{.metadata.name},replicas:{.spec.replicas}" | grep " 1" | head -n 1 | awk '{print $1}')
   # Bump the number of replicas to 6 (+ 1 + 1 == 8 workers)
   oc patch machineset -n ${cluster_api_ns} ${machineset} -p '{"spec":{"replicas":6}}' --type=merge
   wait_until_machineset_scales_up ${cluster_api_ns} ${machineset} 6
@@ -89,20 +87,15 @@ function wait_until_hostname_resolves() {
 
 function install_serverless(){
   header "Installing Serverless Operator"
-  git clone https://github.com/openshift-knative/serverless-operator.git /tmp/serverless-operator
+  # Temporary setup to have Eventing 0.14 installed through Serverless operator
+  # TODO: switch back to master once the next release versions are finalized
+  git clone -b eventing-014-again https://github.com/aliok/serverless-operator.git /tmp/serverless-operator
   # unset OPENSHIFT_BUILD_NAMESPACE as its used in serverless-operator's CI environment as a switch
   # to use CI built images, we want pre-built images of k-s-o and k-o-i
   unset OPENSHIFT_BUILD_NAMESPACE
   /tmp/serverless-operator/hack/install.sh || return 1
   header "Serverless Operator installed successfully"
 }
-
-function teardown_serverless(){
-  header "Tear down Serverless Operator"
-  /tmp/serverless-operator/hack/teardown.sh || return 1
-  header "Serverless Operator uninstalled successfully"
-}
-
 
 function deploy_knative_operator(){
   local COMPONENT="knative-$1"
@@ -134,7 +127,7 @@ function deploy_knative_operator(){
 
   # # Wait until the server knows about the Install CRD before creating
   # # an instance of it below
-  timeout_non_zero 60 '[[ $(oc get crd knative${API_GROUP}s.${API_GROUP}.knative.dev -o jsonpath="{.status.acceptedNames.kind}" | grep -c $KIND) -eq 0 ]]' || return 1
+  timeout 60 '[[ $(oc get crd knative${API_GROUP}s.${API_GROUP}.knative.dev -o jsonpath="{.status.acceptedNames.kind}" | grep -c $KIND) -eq 0 ]]' || return 1
 }
 
 function build_knative_client() {
@@ -150,7 +143,6 @@ function build_knative_client() {
 }
 
 function run_e2e_tests(){
-  TAGS=$1
   header "Running e2e tests"
   failed=0
   # Add local dir to have access to built kn
@@ -164,11 +156,7 @@ function run_e2e_tests(){
   # Add anyuid scc to all authenticated users so e2e tests for --user flag can user any user id
   oc adm policy add-scc-to-group anyuid system:authenticated
 
-  go test \
-    ./test/e2e \
-    -v -timeout=$E2E_TIMEOUT -mod=vendor \
-    -tags="e2e $TAGS" || fail_test
-
+  go_test_e2e -timeout=$E2E_TIMEOUT -mod=vendor ./test/e2e || fail_test
   return $failed
 }
 
@@ -203,87 +191,6 @@ function wait_until_pods_running() {
   return 1
 }
 
-function install_serverless_1_6(){
-  header "Installing Serverless Operator"
-  git clone --branch release-1.6 https://github.com/openshift-knative/serverless-operator.git /tmp/serverless-operator-16
-  # unset OPENSHIFT_BUILD_NAMESPACE as its used in serverless-operator's CI environment as a switch
-  # to use CI built images, we want pre-built images of k-s-o and k-o-i
-  unset OPENSHIFT_BUILD_NAMESPACE
-  /tmp/serverless-operator-16/hack/install.sh || return 1
-  header "Serverless Operator installed successfully"
-}
-
-function create_knative_namespace(){
-  local COMPONENT="knative-$1"
-
-  cat <<-EOF | oc apply -f -
-	apiVersion: v1
-	kind: Namespace
-	metadata:
-	  name: ${COMPONENT}
-	EOF
-}
-
-function deploy_knative_operator(){
-  local COMPONENT="knative-$1"
-  local API_GROUP=$1
-  local KIND=$2
-
-  cat <<-EOF | oc apply -f -
-	apiVersion: v1
-	kind: Namespace
-	metadata:
-	  name: ${COMPONENT}
-	EOF
-  if oc get crd operatorgroups.operators.coreos.com >/dev/null 2>&1; then
-    cat <<-EOF | oc apply -f -
-	apiVersion: operators.coreos.com/v1
-	kind: OperatorGroup
-	metadata:
-	  name: ${COMPONENT}
-	  namespace: ${COMPONENT}
-	EOF
-  fi
-  cat <<-EOF | oc apply -f -
-	apiVersion: operators.coreos.com/v1alpha1
-	kind: Subscription
-	metadata:
-	  name: ${COMPONENT}-subscription
-	  generateName: ${COMPONENT}-
-	  namespace: ${COMPONENT}
-	spec:
-	  source: ${COMPONENT}-operator
-	  sourceNamespace: $OLM_NAMESPACE
-	  name: ${COMPONENT}-operator
-	  channel: alpha
-	EOF
-
-  # # Wait until the server knows about the Install CRD before creating
-  # # an instance of it below
-  timeout_non_zero 60 '[[ $(oc get crd knative${API_GROUP}s.${API_GROUP}.knative.dev -o jsonpath="{.status.acceptedNames.kind}" | grep -c $KIND) -eq 0 ]]' || return 1
-}
-
-function install_knative_eventing(){
-  header "Installing Knative Eventing"
-
-  create_knative_namespace eventing
-
-  # oc apply -n $OLM_NAMESPACE -f knative-eventing.catalogsource-ci.yaml
-  oc apply -n $OLM_NAMESPACE -f $EVENTING_CATALOGSOURCE
-  timeout_non_zero 900 '[[ $(oc get pods -n $OLM_NAMESPACE | grep -c knative-eventing) -eq 0 ]]' || return 1
-  wait_until_pods_running $OLM_NAMESPACE
-
-  # Deploy Knative Operators Eventing
-  deploy_knative_operator eventing KnativeEventing
-
-  # Wait for 5 pods to appear first
-  timeout_non_zero 900 '[[ $(oc get pods -n $EVENTING_NAMESPACE --no-headers | wc -l) -lt 5 ]]' || return 1
-  wait_until_pods_running $EVENTING_NAMESPACE || return 1
-
-  # Assert that there are no images used that are not CI images (which should all be using the $INTERNAL_REGISTRY)
-  # (except for the knative-eventing-operator)
-  #oc get pod -n knative-eventing -o yaml | grep image: | grep -v knative-eventing-operator | grep -v ${INTERNAL_REGISTRY} && return 1 || true
-}
 
 echo ">> Check resources"
 echo ">> - meminfo:"
@@ -303,15 +210,7 @@ failed=0
 
 (( !failed )) && install_serverless || failed=1
 
-(( !failed )) && run_e2e_tests serving || failed=1
-
-(( !failed )) && teardown_serverless || failed=1
-
-(( !failed )) && install_serverless_1_6 || failed=1
-
-(( !failed )) && install_knative_eventing || failed=1
-
-(( !failed )) && run_e2e_tests eventing || failed=1
+(( !failed )) && run_e2e_tests || failed=1
 
 (( failed )) && exit 1
 
