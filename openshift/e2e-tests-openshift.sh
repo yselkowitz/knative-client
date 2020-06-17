2c6bc14bdb541cbb8df9689476e90f0f315a80c0
Navid Shaikh <nshaikh@redhat.com>
2020-06-17 21:22:18 +0530

    Update operator setup (#393)

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -87,9 +87,7 @@ function wait_until_hostname_resolves() {
 
 function install_serverless(){
   header "Installing Serverless Operator"
-  # Temporary setup to have Eventing 0.14 installed through Serverless operator
-  # TODO: switch back to master once the next release versions are finalized
-  git clone -b eventing-014-again https://github.com/aliok/serverless-operator.git /tmp/serverless-operator
+  git clone https://github.com/openshift-knative/serverless-operator.git /tmp/serverless-operator
   # unset OPENSHIFT_BUILD_NAMESPACE as its used in serverless-operator's CI environment as a switch
   # to use CI built images, we want pre-built images of k-s-o and k-o-i
   unset OPENSHIFT_BUILD_NAMESPACE
