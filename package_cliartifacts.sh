73ff755b432963e00e4ec3ed6c39cce146a99480
Navid Shaikh <nshaikh@redhat.com>
2020-11-23 20:55:37 +0530

    Make kn binary executable which goes inside tar file (#523)
    * Make kn binary executable which goes inside tar file
    
      When user extracts the kn tar.gz file, she should get the
      the executable kn binary.
    
    * Make executable for all

--- package_cliartifacts.sh
+++ package_cliartifacts.sh
@@ -20,12 +20,14 @@ pkg_tar() {
     x86_64)
 		  dir=linux
     	mkdir "${OUTDIR}/${dir}"
-    	mv kn-linux-amd64 ${OUTDIR}/${dir}/kn
+      mv kn-linux-amd64 ${OUTDIR}/${dir}/kn
+      chmod +x ${OUTDIR}/${dir}/kn
       ;;
     macos)
 		  dir=macos
     	mkdir "${OUTDIR}/${dir}"
     	mv kn-darwin-amd64 ${OUTDIR}/${dir}/kn
+      chmod +x ${OUTDIR}/${dir}/kn
       ;;
       #uncomment following when we support building mentioned archs
       #aarch64|ppc64le|s390x) dir=linux-${1};;
