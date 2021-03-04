6a62de04367982cb032bc5e9fdef8d68432a3d2a
David Simansky <dsimansk@redhat.com>
2021-03-04 11:24:21 +0100

    Update CI setup to use S-O

--- openshift/e2e-tests-openshift.sh
+++ openshift/e2e-tests-openshift.sh
@@ -10,13 +10,11 @@ failed=0
 # Build binary
 (( !failed )) && build_knative_client || failed=1
 # Run unit tests
-# Temp disabled due to running into OOM
-# (( !failed )) && run_unit_tests || failed=1
-# Serving setup & tests
-(( !failed )) && install_knative_serving_branch "${SERVING_BRANCH}" || failed=1
+ (( !failed )) && run_unit_tests || failed=1
+# Serverless operator setup & tests
+# TODO: change branch to "release-1.17" when available
+(( !failed )) && install_serverless_operator_branch "main" || failed=1
 (( !failed )) && run_client_e2e_tests serving || failed=1
-# Eventing setup & tests
-(( !failed )) && install_knative_eventing_branch "${EVENTING_BRANCH}" || failed=1
 (( !failed )) && run_client_e2e_tests eventing || failed=1
 
 (( failed )) && exit 1
