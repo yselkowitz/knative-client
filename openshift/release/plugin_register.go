7ea4c8ff5aa3f56ddba71b3e1a4ffc2e83aeda78
David Simansky <dsimansk@redhat.com>
2021-11-11 14:45:16 +0100

    Add openshift specific files.

---
+++ openshift/release/plugin_register.go
@@ -0,0 +1,23 @@
+// Copyright © 2020 The OpenShift Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package root
+
+import (
+	// Add #plugins# import here. Don't remove this line, it triggers an automatic replacement.
+)
+
+// RegisterInlinePlugins is an empty function which however forces the
+// compiler to run all init() methods of the registered imports
+func RegisterInlinePlugins() {}
