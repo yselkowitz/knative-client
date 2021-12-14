cf0c36e8dd3705cb25b07448033b9af48485abe5
Chris Suszynski <krzysztof.suszynski@gmail.com>
2021-12-14 16:33:01 +0100

    [0.26] Use sender image promoted by OpenShift CI (#910)
    Align the default sender image to one promoted by OpenShift CI.
    
    This aligns defaults with openshift-knative/serverless-operator#1337
    
    Further refs.:
    
     - openshift/release#24476
     - openshift/release#24549

--- hack/build.sh.d/kn-plugin-event.sh
+++ hack/build.sh.d/kn-plugin-event.sh
@@ -3,8 +3,8 @@
 knEventVersion="$(grep 'knative.dev/kn-plugin-event ' "$(basedir)/go.mod" \
   | head -n 1 \
   | sed -s 's/.* \(v.[\.0-9]*\).*/\1/')"
-readonly upstream_sender_image='gcr.io/knative-releases/kn-event-sender@sha256:8d562077864d6490438ac5b35dd352c6ffa6cd089e81fa06aaf6fe56a0374196'
-readonly sender_image="${KN_PLUGIN_EVENT_SENDER_IMAGE:-${upstream_sender_image}}"
+readonly default_sender_image='registry.ci.openshift.org/knative/release-0.26:client-plugin-event-sender'
+readonly sender_image="${KN_PLUGIN_EVENT_SENDER_IMAGE:-${default_sender_image}}"
 export EXTERNAL_LD_FLAGS="${EXTERNAL_LD_FLAGS:-} \
 -X knative.dev/kn-plugin-event/pkg/metadata.Image=${sender_image} \
 -X knative.dev/kn-plugin-event/pkg/metadata.Version=${knEventVersion}"
